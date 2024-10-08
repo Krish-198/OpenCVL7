a051b98ee8ab7dbbe23ed6fd408ec7a415ef4835
Krish Sethi <95207265+Krish-198@users.noreply.github.com>
2024-10-08 18:58:37 +0530

    Update L7.py

--- L7.py
+++ L7.py
@@ -5,18 +5,20 @@ print(cv2.__version__)
 v=cv2.VideoCapture("video.mp4")
 count=0
 bg=0
+time.sleep(1)
 for i in range(60):
     r,bg=v.read()
     if r==False:
         continue
-mirror=np.flip(bg,axis=1)
+bg=np.flip(bg)
 while(v.isOpened()):
     r,bg1=v.read()
     if not v:
+        print("Error reading")
         break
     count+=1
-    m2=np.flip(bg1,axis=1)
-    hsv=cv2.cvtColor(m2,cv2.COLOR_BGR2HSV)
+    bg1=np.flip(bg1,axis=1)
+    hsv=cv2.cvtColor(bg1,cv2.COLOR_BGR2HSV)
     lowerred=np.array([100,40,40])
     upperred=np.array([100,255,255])
     mask1=cv2.inRange(hsv,lowerred,upperred)
@@ -28,8 +30,8 @@ while(v.isOpened()):
     mask=cv2.morphologyEx(mask,cv2.MORPH_OPEN,np.ones((3,3),np.uint8),iterations=2)
     mask=cv2.dilate(mask,np.ones((3,3),np.uint8),iterations=1)
     mask3=cv2.bitwise_not(mask)
-    result1=cv2.bitwise_and(bg,mirror,mask=mask)
-    result2=cv2.bitwise_and(bg1,m2,mask=mask3)
+    result1=cv2.bitwise_and(bg,bg,mask=mask)
+    result2=cv2.bitwise_and(bg1,bg1,mask=mask3)
     finaloutput=cv2.addWeighted(result1,1,result2,1,0)
     cv2.imshow("Invisible Man",finaloutput)
     k=cv2.waitKey(1)
